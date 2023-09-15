d0e7fa8170b1190b68bb9c77641c248343f45cbb
Adir Silva Filho <tarukagahobenu@gmail.com>
2023-09-15 20:38:41 -0300

    fet: atualiza a Main com o MenuController.

--- src/main/java/ada/poo02/projeto/Main.java
+++ src/main/java/ada/poo02/projeto/Main.java
@@ -5,6 +5,14 @@ import ada.poo02.projeto.controllers.MenuController;
 
 public class Main {
     public static void main(String[] args) {
+        MenuController menuController = new MenuController();
 
+        boolean keep = true;
+
+        System.out.println("Bem vindo a plataforma de aluguel de carro!");
+
+        while (keep) {
+            keep = menuController.execute();
+        }
     }
 }
